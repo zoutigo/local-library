341f5475bc8e537c1668595a158c4ae787c68aee
zoutigo <zoutigo@gmail.com>
2020-09-06 17:10:28 +0200

    connexion to mongodb

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://zoutigo:<password>@cluster0.refc9.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
